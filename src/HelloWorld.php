96af0a2a787b67828df71c4fea3e78faf2257803
vagrant <vagrant@simple-parser>
2017-08-10 17:23:01 +0000

    Add useless file

---
+++ src/HelloWorld.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: felix.welter
+ * Date: 10.08.2017
+ * Time: 19:21
+ */
+
+namespace Jaritt\HelloWorld;
+
+class HelloWorld
+{
+    public static function helloWorld(){
+        return 'What is going on here??? o.O';
+    }
+}
